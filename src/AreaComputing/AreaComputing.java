036e846b7a63d96d2283a89e3b567f1769af87e2
calebm01 <42648514+calebm01@users.noreply.github.com>
2018-09-12 12:55:21 -0600

    Update AreaComputing.java

--- src/AreaComputing/AreaComputing.java
+++ src/AreaComputing/AreaComputing.java
@@ -1,10 +1,31 @@
 package AreaComputing;
 
+import java.util.Scanner;
+/*
+ * Caleb Mouritsen
+ * 9/12/18
+ * compute the area of a circle
+ */
+
 public class AreaComputing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		// step one: enter the radius of a circle
+			double radius; 
+			double pi; 
+			double area;
+			
+			//assign some values
+			System.out.println("Enter a radius: ");
+			radius = input.nextDouble();
+			pi = 3.14159;
+		// step two: compute the area using the following formula 
+		// area = radius*radius*pi
+		area = radius*radius*pi;
+		
+		//step three: display result
+		System.out.println("the area of the circle is: " + area);
 	}
 
 }
